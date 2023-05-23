3568d317473f84d4b00b63e4a27fbc10d9b6abc5
msklnkk <moskalenko-lena@inbox.ru>
2023-05-23 20:41:07 +0400

    добавлен поиск пуль

--- KillEnemies/KillEnemies/KillEnemies.cpp
+++ KillEnemies/KillEnemies/KillEnemies.cpp
@@ -1,7 +1,11 @@
 void KillEnemies(Context& ctx) {
 	for (auto& subject : ctx.current_scene) {
 		if (subject.enemy.enable) {
+			for (auto& bullet : ctx.current_scene) {
+				if (bullet.bullet.enabled) {
 
+				}
+			}
 		}
 	}
 }
