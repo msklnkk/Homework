e1cd7231801db664449a5097592850602aea8cf7
msklnkk <moskalenko-lena@inbox.ru>
2023-05-24 17:29:16 +0400

    добавлены библиотеки и чтение из файла

--- Graph/Graph/Task8_1.cpp
+++ Graph/Graph/Task8_1.cpp
@@ -1,1 +1,9 @@
-//Дан неориентированный граф.
+//Дан неориентированный граф. Вывести количество вершин, смежных с данной.
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <map>
+using namespace std;
+
+ifstream in("list.txt");
