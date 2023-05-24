57d83b881bc914b4672445c989199ad4786efd06
msklnkk <moskalenko-lena@inbox.ru>
2023-05-24 17:37:13 +0400

    добавлен цикл while для ориент. графа

--- Graph/Graph/dz.cpp
+++ Graph/Graph/dz.cpp
@@ -15,7 +15,28 @@ int main() {
 	setlocale(LC_ALL, "RUS");
 	int x, y;
 	map<int, list<int>> Gr;
-	
+
+	while (in.peek() != EOF) {
+		in >> x >> y;
+		auto it_x = Gr.find(x);
+		bool fl = true;
+		if (it_x != Gr.end()) {
+			list<int> a = it_x->second;
+			for (auto it_y = a.begin(); it_y != a.end(); it_y++)
+				if (*it_y == y) {
+					fl = false;
+					break;
+				}
+		}
+		if (fl) {
+			Gr[x].push_back(y);
+			if (Gr.find(y) == Gr.end()) {
+				Gr[y].push_back(0);
+				auto it = Gr.find(y);
+				it->second.erase(it->second.begin());
+			}
+		}
+	}
 
 	return 0;
 }
