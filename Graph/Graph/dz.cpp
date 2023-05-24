8a9cd3842c9ac2193e661096029255efcffa9b7a
msklnkk <moskalenko-lena@inbox.ru>
2023-05-24 17:38:05 +0400

    добавлена функция для обхода в глубину

--- Graph/Graph/dz.cpp
+++ Graph/Graph/dz.cpp
@@ -11,6 +11,23 @@ using namespace std;
 
 ifstream in("list.txt");
 
+void dfs(map<int, list<int>> Gr, int x, bool*& used) {
+	used[x] = true;
+	vector<int> order;
+	for (auto it1 = Gr.begin(); it1 != Gr.end(); it1++) {
+		if (it1->first == x) {
+			list<int> a = it1->second;
+			for (auto it2 = a.begin(); it2 != a.end(); it2++) {
+				if (!used[*it2])
+					dfs(Gr, *it2, used);
+			}
+		}
+	}
+	order.push_back(x);
+	for (int i = 0; i < order.size(); i++)
+		cout << order[i] << " ";
+}
+
 int main() {
 	setlocale(LC_ALL, "RUS");
 	int x, y;
