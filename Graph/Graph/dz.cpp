e809ad639f844c624e39bff2ee0b8d380fc6579a
msklnkk <moskalenko-lena@inbox.ru>
2023-05-24 17:34:36 +0400

    добавлено описание задачи, библиотеки и чтение из файла

---
+++ Graph/Graph/dz.cpp
@@ -0,0 +1,12 @@
+//Дан неориентированный граф. 
+//Вывести Эйлеров путь или Эйлеров цикл, если он существует.
+//Реализовать топологическую сортировку
+
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <map>
+#include <vector>
+using namespace std;
+
+ifstream in("list.txt");
